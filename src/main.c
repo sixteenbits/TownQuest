963dec70ad9081a06596ed498fc46919237b24a9
Emilio Lopez <emiliollbb@gmail.com>
2019-09-28 18:42:24 +0200

    Increase space between people

--- src/main.c
+++ src/main.c
@@ -66,7 +66,7 @@ void handlestate(struct game *game){
         game->loaded_stage=0;
         VDP_resetScreen();
         VDP_drawText("La Vara Estudios", 10 ,13);
-        VDP_drawText("PRESENTS ...", 11 ,15);
+        VDP_drawText("PRESENTS ...", 12 ,15);
     }
     if(game->loaded_stage!=1 && game->current_stage==1) {
         game->loaded_stage=1;
@@ -140,7 +140,7 @@ void init_stage(u16 current_stage, struct game *game) {
     	game->players[i].end_varazo_frame=0;
 	}
 	for(i=0; i<ENEMY_SIZE; i++) {
-		game->enemies[i].y = -1*random()%100;
+		game->enemies[i].y = -1*random()%300;
 		game->enemies[i].x = random()%300;
 		game->enemies[i].vy = 1;
 		game->enemies[i].enabled = 1;
