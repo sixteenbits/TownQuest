b20d1af87502496a446d6814682297d4c7a0be4e
Emilio Lopez <emiliollbb@gmail.com>
2019-09-28 11:26:28 +0200

    Game data structure

--- inc/game.h
+++ inc/game.h
@@ -9,6 +9,7 @@
 #define PERSON_SIZE 10
 
 struct game {
+    // Backgrounds
     Image* background;
     // Players
     struct player players[PLAYERS_SIZE];
@@ -18,6 +19,8 @@ struct game {
     struct person person[ENEMY_SIZE];
     // Stage to display
     u16 current_stage;
+    // Loaded Stage
+    u16 loaded_stage;
 };
 
 #endif
